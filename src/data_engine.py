7fafa82eff85d2e302b266e573121d85c73431cc
Igor Barbosa <igorbarbosapt@gmail.com>
2026-02-11 21:04:57 +0000

    Update data_engine.py

--- src/data_engine.py
+++ src/data_engine.py
@@ -1,21 +1,16 @@
 import pandas as pd
 import yfinance as yf
+import streamlit as st
 
-def fetch_data():
-    url = "https://docs.google.com/spreadsheets/d/1TWfuEvIn9YbSzEyFHKvWWD4XwppHhlj9Cm1RE6BweF8/gviz/tq?tqx=out:csv"
-    try:
-        df = pd.read_csv(url)
-        df.columns = [c.strip() for c in df.columns]
-        return df.dropna(subset=['Ativo'])
-    except:
-        return None
-
+@st.cache_data(ttl=600) # Isso impede o Yahoo de te bloquear por 10 minutos
 def sync_prices(df):
     tickers = df['Ativo'].unique().tolist()
+    # Puxa os dados uma única vez e guarda na memória
     data = yf.download(tickers, period="1d", progress=False)['Close']
+    
     p_dict = {t: float(data[t].iloc[-1] if len(tickers) > 1 else data.iloc[-1]) for t in tickers}
     df['Preço Atual'] = df['Ativo'].map(p_dict)
     df['Patrimônio'] = df['QTD'] * df['Preço Atual']
     df['Valorização %'] = (df['Preço Atual'] / df['Preço Médio'] - 1) * 100
     return df
-  
+    
