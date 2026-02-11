2e5e023807f76d342b52b3eee41fac9740172394
Igor Barbosa <igorbarbosapt@gmail.com>
2026-02-11 21:16:07 +0000

    Delete components directory

--- components/cards.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def display_main_metrics(future_value, real_future_value):
-    col1, col2 = st.columns(2)
-    col1.metric("Valor Futuro (nominal)", f"R$ {future_value:,.2f}")
-    col2.metric("Valor Futuro (ajustado pela inflação)", f"R$ {real_future_value:,.2f}")
